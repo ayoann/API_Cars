2ae0db3f478a948d4dd0d4d3a497e6093128a231
yalbert <yalbert@talend.com>
2019-08-19 15:17:27 +0200

    Create script to create garages and cars

---
+++ Script/ScriptPostgresqlCreateCars.sql
@@ -0,0 +1,13 @@
+------------------------------------------------------------
+-- Garages 1
+------------------------------------------------------------
+insert into cars(registration, brand, model, color, date_commissioning, price, id_garages) values('CQ-56-FD','Peugeot','208','red','2013-08-19T12:43:09.910Z','6000','1');
+insert into cars(registration, brand, model, color, date_commissioning, price, id_garages) values('DF-KJ-56','Renaud','Modus','green','2003-08-19T12:43:09.910Z','2500','1');
+insert into cars(registration, brand, model, color, date_commissioning, price, id_garages) values('GT-89-YU','Opel','Vectra', 'white', '2016-08-19T12:43:09.910Z', '8000', '1');
+
+------------------------------------------------------------
+-- Garages 2
+------------------------------------------------------------
+insert into cars(registration, brand, model, color, date_commissioning, price, id_garages) values('C5-89-52','Peugeot','108', 'blue', '2013-08-19T12:43:09.910Z', '8000', '2');
+insert into cars(registration, brand, model, color, date_commissioning, price, id_garages) values('DE-23-CV','Renaud','Megane', 'black', '2001-08-19T12:43:09.910Z', '1000', '2');
+insert into cars(registration, brand, model, color, date_commissioning, price, id_garages) values('ZE-14-PO','Opel','Corsa', 'white', '2015-08-19T12:43:09.910Z', '7500', '2');
